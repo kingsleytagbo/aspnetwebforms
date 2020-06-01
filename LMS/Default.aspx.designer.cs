a78c738ca0c1d2ae5d09d2bcd1569572e264bbe7
kingsley Tagbo <kingsleytagbo@gmail.com>
2020-06-01 08:43:20 -0500

    Users & Registration

--- LMS/Default.aspx.designer.cs
+++ LMS/Default.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace LMS
 
     public partial class _Default
     {
-
-        /// <summary>
-        /// login control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Login login;
-
-        /// <summary>
-        /// MessageAuthentication control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl MessageAuthentication;
     }
 }
